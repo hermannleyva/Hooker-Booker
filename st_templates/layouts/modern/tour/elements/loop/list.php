e2239b0d8e4582f17f99352b94ac8c82c7d9b94a
lakshkhamesra <lakshkhamesra@gmail.com>
2021-11-08 18:54:07 +0530

    Update list.php

--- st_templates/layouts/modern/tour/elements/loop/list.php
+++ st_templates/layouts/modern/tour/elements/loop/list.php
@@ -25,13 +25,13 @@ $url = get_author_posts_url( $author_id );
                 <?php if(is_user_logged_in()){ ?>
                     <?php $data = STUser_f::get_icon_wishlist();?>
                     <div class="service-add-wishlist login <?php echo ($data['status']) ? 'added' : ''; ?>" data-id="<?php echo get_the_ID(); ?>" data-type="<?php echo get_post_type(get_the_ID()); ?>" title="<?php echo ($data['status']) ? __('Remove from wishlist', 'traveler') : __('Add to wishlist', 'traveler'); ?>">
-                        <i class="fa fa-heart"></i>
+<!--                         <i class="fa fa-heart"></i> -->
                         <div class="lds-dual-ring"></div>
                     </div>
                 <?php }else{ ?>
                     <a href="" class="login" data-toggle="modal" data-target="#st-login-form">
                         <div class="service-add-wishlist" title="<?php echo __('Add to wishlist', 'traveler'); ?>">
-                            <i class="fa fa-heart"></i>
+<!--                             <i class="fa fa-heart"></i> -->
                             <div class="lds-dual-ring"></div>
                         </div>
                     </a>
