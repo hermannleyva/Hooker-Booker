80a4a5fdf01eb6224f1970c7c5cf92e992be020a
lakshkhamesra <lakshkhamesra@gmail.com>
2021-11-08 17:52:59 +0530

    Update list.php

--- st_templates/layouts/modern/tour/elements/loop/list.php
+++ st_templates/layouts/modern/tour/elements/loop/list.php
@@ -11,7 +11,7 @@ $url=st_get_link_with_search(get_permalink(),array('check_in','check_out','durat
         <div class="col-sm-4 thumb-wrapper">
             <div class="thumb">
                 <?php $author_id=$post->post_author; ?>
-                <?php echo get_avatar($author_id, 450); ?>
+                <?php echo get_avatar($author_id, 270); ?>
                 <?php if(!empty( $info_price['discount'] ) and $info_price['discount']>0 and $info_price['price_new'] >0) { ?>
                     <?php echo STFeatured::get_sale($info_price['discount']); ?>
                 <?php } ?>
